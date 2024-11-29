4c2f7c4ee37b6d72f725d2bbb5d508022e0dfe4a
Elli Manavi <elmira_manavi@aol.de>
2024-11-29 19:04:16 +0100

    replace serp api by google maps api

--- src/crawler/serp_maps.py
+++ src/crawler/serp_maps.py
@@ -1,24 +1,62 @@
 from src.configuration.config import SERP_API_KEY
 from serpapi import GoogleSearch
 
+# maps type ids
+relevant_locations = [
+    "art_gallery",
+    "auditorium",
+    "museum",
+    "performing_arts_theater",
+    "amphitheatre",
+    "amphitheatre",
+    "amusement_center",
+    "amusement_park",
+    "banquet_hall",
+    "childrens_camp",
+    "comedy_club",
+    "community_center",
+    "concert_hall",
+    "convention_center",
+    "cultural_center",
+    "dance_hall",
+    "event_venue",
+    "karaoke",
+    "night_club",
+    "opera_house",
+    "philharmonic_hall",
+    "planetarium",
+    "library",
+    "church",
+    "hindu_temple",
+    "mosque",
+    "synagogue"
+]
+
+
 params = {
     "engine": "google_maps",
     "q": "",
     "type": "search",
     "api_key": SERP_API_KEY ,
-    "ll": "@49.4540304,11.101698,14z"  # coordinates for Nuremberg with 15 zoom in
+    # "ll": "@49.4540304,11.101698,14z"  # coordinates for Nuremberg with 15 zoom in
 }
 
 
-def get_maps_results(search_keywords ):
+def get_maps_results(search_query ):
     results = []
-    for keyword in search_keywords:
-        params["q"] = keyword
-        search = GoogleSearch(params)
-        local_results = search.get_dict()["local_results"]
-        for location in local_results:
-            if "website" in location and location["website"] not in results:
-                results.append(location["website"])
-                # print(location["website"])
+
+    params["q"] = search_query
+    search = GoogleSearch(params)
+    search_dict = search.get_dict()
+    if "local_results" not in search_dict:
+        return results
+    local_results = search_dict["local_results"]
+
+    for location in local_results:
+        if "website" in location and location["website"] not in results:
+
+            results.append(location["website"])
     return results
 
+
+# print(get_maps_results("Konzerte Nuernberg"))
